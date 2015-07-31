dd0a50a8458a94fdffdc67a832a6246184983d84
Laura Nawrocki <lwn2@aol.com>
2015-07-31 02:38:37 -0400

    bug fixes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base
-  has_single :post, dependent: :destroy
   belongs_to :post
   belongs_to :user
 end
--- db/comment_data.rb
+++ db/comment_data.rb
@@ -1,8 +1,17 @@
 def get_comment_data
-  comment_data =
-    post1=>[{:date=>"Yesterday", :commenttext=>"Hi, mom!"},
-    {:date=>"Friday", :commenttext=>"First!"},
-    {:date=>"Tuesday", :commenttext=>"You stink!"},
-    {:date=>"Saturday", commenttext=>"excellent!"}]
+  comment_data = {
+    "post1" => {
+                date: "Yesterday",
+                commenttext: "Hi, mom!"
+              },
+    "post2" => {
+                date: "Friday",
+                commenttext: "First!"
+      },
+    "post3" => {
+                date: "Tuesday",
+                commenttext: "You stink!"
+              }
+    }
   return comment_data
 end
