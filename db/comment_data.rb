bc62b539ba2f5935ab4b7e06b8b0dc91512ef82b
Laura Nawrocki <lwn2@aol.com>
2015-07-31 02:07:41 -0400

    deleting -myblog in config/application.rb
    because gave error in rails?

---
+++ db/comment_data.rb
@@ -0,0 +1,8 @@
+def get_comment_data
+  comment_data =
+    post1=>[{:date=>"Yesterday", :commenttext=>"Hi, mom!"},
+    {:date=>"Friday", :commenttext=>"First!"},
+    {:date=>"Tuesday", :commenttext=>"You stink!"},
+    {:date=>"Saturday", commenttext=>"excellent!"}]
+  return comment_data
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# data
+require_relative './comment_data.rb'
+require_relative './post_data.rb'
+
+Comment.destroy_all
+Post.destroy_all
+
+comment_data = get_comment_data()
+post_data = get_post_data()
+
+comment_data.each_pair do |comment_name, comments|
+  info = post_data[post_title]
+  current_post = post.create!({
+    title:         info[:title],
+    date:    info[:date],
+    posttext:  info[:posttext]
+  })
+
+  comments.each do |comment|
+    Comment.create!({
+      date:        comment[:date],
+      commenttext:  comment[:commenttext],
+      post:   current_post
+    })
+  end
+end
